ae64d2c789a8df2708161f2162ca68c3504042d6
Patrick-MarequesV <patrick_1997marques@hotmail.com>
2022-08-25 13:28:25 -0300

    Add Ex1

---
+++ 1000Devs_Encontro13_Algoritimos/Ex2.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Ex2 {
+    public static void main(String[] args) throws IOException{
+        Scanner input = new Scanner(System.in);
+
+        
+
+    }
+    
+}
