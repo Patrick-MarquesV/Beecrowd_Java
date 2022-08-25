4070b2a680a7ed83fe4f3efe0dc1df93a93a2f3b
Patrick-MarequesV <patrick_1997marques@hotmail.com>
2022-08-25 12:30:11 -0300

    add Java exercices

--- 1000Devs/Encontro13_Algoritimos/Ex1.java
+++ 1000Devs/Encontro13_Algoritimos/Ex1.java
@@ -5,10 +5,12 @@ informar as dimensões em metros (frente e lateral) do terreno além do valor co
 metro quadrado.
  */
 
+ // teste de alteraçãoS
+
 import java.io.IOException;
 import java.util.Scanner;
 
- public class Main{
+ public class Ex1{
     public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
 
@@ -16,12 +18,12 @@ import java.util.Scanner;
 
         System.out.print("Quantos metros o terreno possui de frente: ");
         dimFrente = input.nextFloat();
-        Sytem.out.print("\nQUantos metros o terreno possui de lateral ");
+        System.out.print("QUantos metros o terreno possui de lateral ");
         dimLateral = input.nextFloat();
-        System.out.print("\nInforme o valor do metro quadrado: ");
+        System.out.print("Informe o valor do metro quadrado: ");
         valorMetro = input.nextFloat();
 
-        System.out.printf("\nArea total do terreno de %.2f mts de frente com %.2f mts de lateral e: %.2f mts\n", dimFrente, dimLateral, dimLateral*dimFrente);
+        System.out.printf("Area total do terreno de %.2f mts de frente com %.2f mts de lateral e: %.2f mts\n", dimFrente, dimLateral, dimLateral*dimFrente);
         System.out.printf("O valor desse terreno e: R$ %.2f\n", dimFrente*dimLateral*valorMetro);
 
 
