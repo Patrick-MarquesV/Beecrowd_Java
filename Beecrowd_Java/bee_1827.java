9554c4db78452d9e2899bb50dc64a2190914949c
Patrick-MarequesV <patrick_1997marques@hotmail.com>
2022-09-05 19:27:12 -0300

    Accepted bee 1827

--- Beecrowd_Java/bee_1827.java
+++ Beecrowd_Java/bee_1827.java
@@ -6,28 +6,37 @@ public class bee_1827{
     public static void main(String[] args) throws IOException{
         Scanner input = new Scanner(System.in);
 
-        StringBuilder str = new StringBuilder();
-
         int arrSize, value=0;
 
         while(input.hasNext()){
-            arrSize = input.nextInt();
+            StringBuilder str = new StringBuilder();
 
+            arrSize = input.nextInt();
+            
             for(int i = 0; i<arrSize; i++){
                 for(int j = 0; j<arrSize; j++){
+                    
+                    value = 0;
 
                     if(i==j){
+                        value = 2;
+                    }
+                    if(i+j==arrSize-1){
+                        value = 3;
+                    }
+                    if((j>=(arrSize/3) && i>=(arrSize/3))&&(i<arrSize-(arrSize/3) && j<arrSize-(arrSize/3))){
                         value = 1;
                     }
-
-
+                    if(i==arrSize/2 && j==arrSize/2){
+                        value = 4;
+                    }
 
                     str.append(value);
                 }
+                str.append("\n");
             }
-            str.append("\n");
+            System.out.print(str.toString());
+            System.out.println();
         }
-        System.out.print(str.toString());
-
     }
 }
